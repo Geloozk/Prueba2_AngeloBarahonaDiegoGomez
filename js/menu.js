bccdf3af9e702a31d8aad0a7c0df4866f8b38ad0
Angelo Barahona <ange.barahona@duocuc.cl>
2021-06-02 08:28:36 -0400

    menu

--- js/menu.js
+++ js/menu.js
@@ -6,6 +6,8 @@ const putInfo = document.getElementById("putInfo")
 const putForm = document.getElementById("formulario")
 const putCont = document.getElementById("contacto")
 
+const button = document.getElementById("button")
+
     $.getJSON('https://mindicador.cl/api', function(data) {
         var dailyIndicators = data;
         $("<p/>", {
@@ -21,7 +23,7 @@ putInfo.innerHTML =
 
 `        <div id="todoInfo" class="wow fadeIn">
 
-    <h2>Formulario Único de Acreditación Socioeconómica (FUAS)</h2>
+    <h1>Formulario Único de Acreditación Socioeconómica (FUAS)</h1>
     <ul>
     <li>Gratuidad.</li>
     <li>Beca Nuevo Milenio.</li>
@@ -52,7 +54,7 @@ informacion.onclick = () => {
         console.log('Error al consumir la API!');
     });
     putInfo.innerHTML = `        <div id="todoInfo" class="wow fadeIn">
-    <h2>Formulario Único de Acreditación Socioeconómica (FUAS)</h2>
+    <h1>Formulario Único de Acreditación Socioeconómica (FUAS)</h1>
     <ul>
     <li>Gratuidad.</li>
     <li>Beca Nuevo Milenio.</li>
@@ -101,20 +103,293 @@ contacto.onclick = () => {
             </div>
         </div>
         </center>
-
-   
-   
     </div>
 ` 
+const button = document.getElementById("button")
+button.onclick = () => {
+    alert("Gracias por ponerte en contacto con nosotros")
+}
+
+
 }
 
 formulario.onclick = () => {
 
     putInfo.innerHTML = `        <div id="todoInfo" class="wow fadeIn">
     <h2>Formulario</h2>
-  
-   
-   
+    <h2>Nombre</h2>
+            <input type="text" placeholder="Nombre" style="color: black; height: 4vh;"/>
+
+    <h2>Rut</h2>
+            <input type="text" placeholder="Rut" style="color: black; height: 4vh;"/>
+
+
+
+
+        <h2>Ingrese su Estado Civil</h2>
+      <input type="text" placeholder="Estado Civil" style="color: black; height: 4vh;"/>
+        <h2> Nacionalidad </h2>
+      <select type="text" placeholder="Nacionalidad" style="color: white ; height: 4vh;"/>
+            <option value="chilena">Chilena</option>
+        <option value="extranjera">Extranjera con Permanencia Definitiva</option>
+        <option value="fiat">Extranjera con residencia</option></select>
+
+             <h2>Indice su Actividad</h2>
+
+        <input type="text" placeholder="Actividad" style="color: black; height: 4vh;"/>
+
+        <h2>¿Cursaste los 2 últimos años de enseñanza escolar en el extranjero?</h2>
+             <select type="text" placeholder="Selecciona una" style="color: white ; height: 4vh;"/>
+        <option value="si">Sí</option>
+        <option value="no">No</option></select>
+
+
+            <h2>¿Tienes alguna discapacidad?</h2>
+             <select type="text" style="color: white ; height: 4vh;"/>
+        <option value="si">Sí</option>
+        <option value="no">No</option></select>
+                        <h2>¿A qué pueblo perteneces?</h2>
+
+              <input type="text" placeholder="Pueblo" style="color: black; height: 4vh;"/>
+
+        
+  <h2>Cuenta de ahorro para estudios de educación superior</h2>
+             <select type="text" placeholder="Selecciona una" style="color: white ; height: 4vh;"/>
+        <option value="si">Sí</option>
+        <option value="no">No</option></select>
+
+                <h2>Antecedentes Académicos</h2>
+
+
+                <h2>Promedio de Notas de Enseñanza Media</h2>
+
+                <input type="text" placeholder="Promedio" style="color: black; height: 4vh;"/>
+                
+                <h2>Institución de educación superior en la que estudias</h2>
+                <input type="text" placeholder="Institución" style="color: black; height: 4vh;"/>
+
+          <h2>Último año de matrícula en educación superior</h2>
+                <input type="text" placeholder="Año" style="color: black; height: 4vh;"/>
+
+                <h2>Integrantes del Grupo Familiar</h2>
+
+                <h2>Nombre</h2>
+                <input type="text" placeholder="Nombre" style="color: black; height: 4vh;"/>
+
+
+                <h2>Apellido Paterno</h2>
+                <input type="text" placeholder="Apellido" style="color: black; height: 4vh;"/>
+
+
+
+                <h2>Apellido Materno</h2>
+                <input type="text" placeholder="Apellido" style="color: black; height: 4vh;"/>
+
+
+
+                <h2>Rut</h2>
+                <input type="text" placeholder="Rut" style="color: black; height: 4vh;"/>
+
+
+                <h2>Edad</h2>
+                <input type="text" placeholder="Edad" style="color: black; height: 4vh;"/>
+
+
+                <h2>Estado Civil</h2>
+                <input type="text" placeholder="Estado Civil" style="color: black; height: 4vh;"/>
+
+
+                <h2>Parentesco</h2>
+                <input type="text" placeholder="Parentesco" style="color: black; height: 4vh;"/>
+
+                <h2>Previsión</h2>
+                <input type="text" placeholder="Previsión" style="color: black; height: 4vh;"/>
+
+                <h2>Previsión Salud</h2>
+                <input type="text" placeholder="Previsión Salud" style="color: black; height: 4vh;"/>
+
+                <h2>Actividad</h2>
+                <input type="text" placeholder="Actividad" style="color: black; height: 4vh;"/>
+
+                 <h2>Nivel de Estudios</h2>
+                <input type="text" placeholder="Nivel de Estudios" style="color: black; height: 4vh;"/>
+
+                    <h2>Nombre</h2>
+                <input type="text" placeholder="Nombre" style="color: black; height: 4vh;"/>
+
+
+                <h2>Apellido Paterno</h2>
+                <input type="text" placeholder="Apellido" style="color: black; height: 4vh;"/>
+
+
+
+                <h2>Apellido Materno</h2>
+                <input type="text" placeholder="Apellido" style="color: black; height: 4vh;"/>
+
+
+
+                <h2>Rut</h2>
+                <input type="text" placeholder="Rut" style="color: black; height: 4vh;"/>
+
+
+                <h2>Edad</h2>
+                <input type="text" placeholder="Edad" style="color: black; height: 4vh;"/>
+
+
+                <h2>Estado Civil</h2>
+                <input type="text" placeholder="Estado Civil" style="color: black; height: 4vh;"/>
+
+
+                <h2>Parentesco</h2>
+                <input type="text" placeholder="Parentesco" style="color: black; height: 4vh;"/>
+
+                <h2>Previsión</h2>
+                <input type="text" placeholder="Previsión" style="color: black; height: 4vh;"/>
+
+                <h2>Previsión Salud</h2>
+                <input type="text" placeholder="Previsión Salud" style="color: black; height: 4vh;"/>
+
+                <h2>Actividad</h2>
+                <input type="text" placeholder="Actividad" style="color: black; height: 4vh;"/>
+
+                 <h2>Nivel de Estudios</h2>
+                <input type="text" placeholder="Nivel de Estudios" style="color: black; height: 4vh;"/>
+
+
+                    <h2>Nombre</h2>
+                <input type="text" placeholder="Nombre" style="color: black; height: 4vh;"/>
+
+
+                <h2>Apellido Paterno</h2>
+                <input type="text" placeholder="Apellido" style="color: black; height: 4vh;"/>
+
+
+
+                <h2>Apellido Materno</h2>
+                <input type="text" placeholder="Apellido" style="color: black; height: 4vh;"/>
+
+
+
+                <h2>Rut</h2>
+                <input type="text" placeholder="Rut" style="color: black; height: 4vh;"/>
+
+
+                <h2>Edad</h2>
+                <input type="text" placeholder="Edad" style="color: black; height: 4vh;"/>
+
+
+                <h2>Estado Civil</h2>
+                <input type="text" placeholder="Estado Civil" style="color: black; height: 4vh;"/>
+
+
+                <h2>Parentesco</h2>
+                <input type="text" placeholder="Parentesco" style="color: black; height: 4vh;"/>
+
+                <h2>Previsión</h2>
+                <input type="text" placeholder="Previsión" style="color: black; height: 4vh;"/>
+
+                <h2>Previsión Salud</h2>
+                <input type="text" placeholder="Previsión Salud" style="color: black; height: 4vh;"/>
+
+                <h2>Actividad</h2>
+                <input type="text" placeholder="Actividad" style="color: black; height: 4vh;"/>
+
+                 <h2>Nivel de Estudios</h2>
+                <input type="text" placeholder="Nivel de Estudios" style="color: black; height: 4vh;"/>
+
+
+                    <h2>Nombre</h2>
+                <input type="text" placeholder="Nombre" style="color: black; height: 4vh;"/>
+
+
+                <h2>Apellido Paterno</h2>
+                <input type="text" placeholder="Apellido" style="color: black; height: 4vh;"/>
+
+
+
+                <h2>Apellido Materno</h2>
+                <input type="text" placeholder="Apellido" style="color: black; height: 4vh;"/>
+
+
+
+                <h2>Rut</h2>
+                <input type="text" placeholder="Rut" style="color: black; height: 4vh;"/>
+
+
+                <h2>Edad</h2>
+                <input type="text" placeholder="Edad" style="color: black; height: 4vh;"/>
+
+
+                <h2>Estado Civil</h2>
+                <input type="text" placeholder="Estado Civil" style="color: black; height: 4vh;"/>
+
+
+                <h2>Parentesco</h2>
+                <input type="text" placeholder="Parentesco" style="color: black; height: 4vh;"/>
+
+                <h2>Previsión</h2>
+                <input type="text" placeholder="Previsión" style="color: black; height: 4vh;"/>
+
+                <h2>Previsión Salud</h2>
+                <input type="text" placeholder="Previsión Salud" style="color: black; height: 4vh;"/>
+
+                <h2>Actividad</h2>
+                <input type="text" placeholder="Actividad" style="color: black; height: 4vh;"/>
+
+                 <h2>Nivel de Estudios</h2>
+                <input type="text" placeholder="Nivel de Estudios" style="color: black; height: 4vh;"/>
+
+                <h2> Dirección del Grupo Familiar
+
+                 <h2>Dirección</h2>
+                <input type="text" placeholder="Dirección" style="color: black; height: 4vh;"/>
+
+
+                 <h2>Número</h2>
+                <input type="text" placeholder="Número" style="color: black; height: 4vh;"/>
+
+
+                 <h2>Departamento</h2>
+                <input type="text" placeholder="Departamento" style="color: black; height: 4vh;"/>
+
+
+                 <h2>Población o Villa</h2>
+                <input type="text" placeholder="Población o Villa" style="color: black; height: 4vh;"/>
+                
+                <h2>Región</h2>
+                <input type="text" placeholder="Región" style="color: black; height: 4vh;"/>
+
+                          <h2>Comuna</h2>
+                <input type="text" placeholder="Comuna" style="color: black; height: 4vh;"/>
+
+                      <h2>Ciudad</h2>
+                <input type="text" placeholder="Ciudad" style="color: black; height: 4vh;"/>
+
+            <h2> Teléfono <h2>
+
+                <h2>Código de Área</h2>
+                <input type="text" placeholder="Código" style="color: black; height: 4vh;"/>
+
+    <h2>Teléfono Fijo</h2>
+                <input type="text" placeholder="Teléfono" style="color: black; height: 4vh;"/>
+
+
+    <h2>Celular</h2>
+                <input type="text" placeholder="Celular" style="color: black; height: 4vh;"/>
+
+    <input type="submit" id="envForm" value="Enviar Formulario" style="color: black; height: 4vh;"/>
+
+
+
     </div>
 ` 
+
+const envForm = document.getElementById("envForm")
+
+envForm.onclick = () => {
+    alert("Postulación Realizada Correctamente.")
+    window.location = "home.html"
+
+}
+
 }
