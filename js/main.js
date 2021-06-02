6eade583c3dc723d4642e2dc7c2422d44cc02d18
Angelo Barahona <ange.barahona@duocuc.cl>
2021-06-02 03:22:02 -0400

    Diseños

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('formulario');
+
+
+form.onsubmit = (e) => {
+e.preventDefault();
+
+const usuario = document.getElementById("usuario").value
+const correo = document.getElementById("correo").value
+const button = document.getElementById("button")
+button.onclick = () => {
+
+    if (usuario === "hola" && correo === "hola@hola.cl"){
+        window.location="home.html"
+    }
+    else{
+     
+        alert("Debe ingresar los datos correctamente.")
+        form.innerHTML = ` <h3>Iniciar Sesión</h3>
+            <input id="usuario" type="text" placeholder="Ingrese su Usuario"/>
+            <input id="correo" type="text" placeholder="Ingrese su Correo"/>
+            <input id="button" type="submit" value="Ingresar"/>`
+    }
+}
+
+   }
